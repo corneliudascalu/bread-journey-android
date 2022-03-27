3565f9b6d620126d081f74fb33e23e5cb2c832ac
Corneliu Dascălu <corneliu.dascalu@gmail.com>
2022-03-27 19:06:28 +0300

    Set up collapsing toolbar layout with header photo

--- app/src/main/java/com/corneliudascalu/bakerjourney/log/LogEntryFragment.kt
+++ app/src/main/java/com/corneliudascalu/bakerjourney/log/LogEntryFragment.kt
@@ -1,11 +1,13 @@
 package com.corneliudascalu.bakerjourney.log
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.corneliudascalu.bakerjourney.R
 import com.corneliudascalu.bakerjourney.databinding.FragmentLogEntryBinding
 import org.koin.core.component.KoinComponent
@@ -26,12 +28,28 @@ class LogEntryFragment : Fragment(R.layout.fragment_log_entry), KoinComponent {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentLogEntryBinding.bind(view)
+        setHasOptionsMenu(true)
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         logEntryId?.also {
-            binding.toolbar.title = logRepository[it]?.name
-            binding.toolbar.subtitle = logRepository[it]?.description
+            val logEntry = logRepository[it]
+            binding.toolbar.title = logEntry?.name
+            binding.collapsingToolbar.title = logEntry?.description
+            binding.toolbar.subtitle = logEntry?.description
             binding.name.text =
-                logRepository[it]?.name + logRepository[it]?.name + logRepository[it]?.name + logRepository[it]?.name + logRepository[it]?.name + logRepository[it]?.name
+                logEntry?.name + logEntry?.name + logEntry?.name + logEntry?.name + logEntry?.name + logEntry?.name
+            Glide.with(binding.root)
+                .load(logEntry?.photoUrl)
+                .centerCrop()
+                .into(binding.headerPhoto)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            parentFragmentManager.popBackStack()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
