6eca901219fea3040c765b61f612915c02e5b70d
Corneliu Dascălu <corneliu.dascalu@gmail.com>
2022-03-27 19:30:09 +0300

    Display partial log entry data in rough UI

--- app/src/main/java/com/corneliudascalu/bakerjourney/log/LogEntryFragment.kt
+++ app/src/main/java/com/corneliudascalu/bakerjourney/log/LogEntryFragment.kt
@@ -1,15 +1,21 @@
 package com.corneliudascalu.bakerjourney.log
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.core.view.LayoutInflaterCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.corneliudascalu.bakerjourney.R
+import com.corneliudascalu.bakerjourney.Step
 import com.corneliudascalu.bakerjourney.databinding.FragmentLogEntryBinding
+import com.corneliudascalu.bakerjourney.databinding.ListItemLogEntryBinding
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -33,20 +39,44 @@ class LogEntryFragment : Fragment(R.layout.fragment_log_entry), KoinComponent {
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         logEntryId?.also {
             val logEntry = logRepository[it]
-            binding.toolbar.title = logEntry?.name
-            binding.collapsingToolbar.title = logEntry?.description
-            binding.toolbar.subtitle = logEntry?.description
-            binding.name.text =
-                logEntry?.name + logEntry?.name + logEntry?.name + logEntry?.name + logEntry?.name + logEntry?.name
+            requireNotNull(logEntry)
+            binding.toolbar.title = logEntry.name
+            binding.collapsingToolbar.title = logEntry.description
+            binding.toolbar.subtitle = logEntry.description
+            binding.name.text = logEntry.name
+            binding.description.text = logEntry.description
+            // TODO Implement a proper adapter and recycler view
+            for (step in logEntry.steps) {
+                val childView = when (step) {
+                    is Step.CheckableStep -> {
+                        val checkBox = CheckBox(binding.root.context)
+                        checkBox.text = step.step.comment
+                        checkBox
+                    }
+                    is Step.IngredientStep -> {
+                        val stepBinding = ListItemLogEntryBinding.inflate(LayoutInflater.from(binding.root.context))
+                        stepBinding.mtrlListItemText.text = step.ingredient.toString()
+                        stepBinding.mtrlListItemSecondaryText.text = step.comment
+                        stepBinding.root
+                    }
+                    is Step.TextStep -> {
+                        val textView = TextView(binding.root.context)
+                        textView.text = step.comment
+                        textView
+                    }
+                }
+                binding.stepsLayout.addView(childView)
+            }
+
             Glide.with(binding.root)
-                .load(logEntry?.photoUrl)
+                .load(logEntry.photoUrl)
                 .centerCrop()
                 .into(binding.headerPhoto)
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home){
+        if (item.itemId == android.R.id.home) {
             parentFragmentManager.popBackStack()
             return true
         }
